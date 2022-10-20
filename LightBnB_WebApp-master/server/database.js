dcf88243c805454a15ac2c79b4adfe0f5087c470
Michaeltcdavis <michaeltcdavis@gmail.com>
2022-10-20 21:34:28 +0000

    add customized search to get properties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -113,11 +113,52 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = (options, limit = 10) => {
+  const queryParams = [];
+  let queryString = `
+    SELECT properties.*, AVG(rating) AS average_rating
+    FROM properties
+    LEFT JOIN property_reviews ON properties.id=property_id
+    `;
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length}`;
+  }
+  if (options.owner_id) {
+    if (queryParams.length > 0) {
+      queryString += ` AND`
+    } else {
+      queryString += `WHERE`
+    }
+    queryParams.push(`${options.owner_id}`);
+    queryString += ` owner_id = $${queryParams.length}`;
+  }
+  if (options.minimum_price_per_night) {
+    if (queryParams.length > 0) {
+      queryString += ` AND`
+    } else {
+      queryString += `WHERE`
+    }
+    queryParams.push(options.minimum_price_per_night * 100);
+    queryString += ` cost_per_night >= $${queryParams.length}`;
+  } 
+  if (options.maximum_price_per_night) {
+    if (queryParams.length > 0) {
+      queryString += ` AND`
+    } else {
+      queryString += `WHERE`
+    }
+    queryParams.push(options.maximum_price_per_night * 100);
+    queryString += ` cost_per_night <= $${queryParams.length}`;
+  }
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+  console.log(queryString, queryParams);
   return pool
-    .query(`
-    SELECT * FROM properties
-    LIMIT $1`,
-      [limit])
+    .query(queryString, queryParams)
     .then((result) => {
       console.log(result.rows);
       return result.rows;
